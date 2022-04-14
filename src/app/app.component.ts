6b7f5589d4742f36bbf8f1274a9510cdfdf6e4c9
sdi1900012 <sdi1900012@di.uoa.gr>
2022-04-14 18:22:54 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "./user";
+import {UserService} from "./user.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bidderappFront';
+export class AppComponent implements OnInit {
+  public users: User[] | undefined;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe((response: User[]) => {
+      this.users = response;
+    }, (error: HttpErrorResponse) => {
+      alert(error.message);
+    })
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "./user";
+import {environment} from "../environments/environment";
 
-
+//same as manually in providers
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
