eb5940e9d12082457d47c999d7d031f66023a154
sdi1900012 <sdi1900012@di.uoa.gr>
2022-04-14 17:25:54 +0300

    first commite

---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  username: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  address: string;
+  postNumber: string;
+  afm: string;
+  imageUrl: string;
+}
